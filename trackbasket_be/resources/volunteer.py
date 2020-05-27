943d7da4696f66a1bc1b88856edfe4703daf4bea
Maria Ronauli <mronauli@gmail.com>
2020-05-27 11:55:02 -0600

    build out post and get actions in volunteers resource
    Co-Authored-By: Alexis Dumortier <dumortier.alexis@gmail.com>
    Co-Authored-By: Alexis Dumortier <alexis.dumortier@free.fr>

--- trackbasket_be/resources/volunteer.py
+++ trackbasket_be/resources/volunteer.py
@@ -1,11 +1,17 @@
 from flask_restful import Resource, reqparse, request
+from models.volunteer import Volunteer, db
+from models.basemodel import BaseModel
 
-class Volunteer(Resource):
+class Volunteers(Resource):
 
   def get(self,id):
-    return {'volunteer': id}
+    volunteer = db.session.query(Volunteer).get(id)
+    return {'volunteer': volunteer.name}
 
-  def post(self,id):
+  def post(self, id):
     request_data = request.json
+    volunteer = Volunteer(name=request_data['name'], phone_number=request_data['phone_number'])
+    db.session.add(volunteer)
+    db.session.commit()
     # write to the database
     return {'volunteer': request_data}
