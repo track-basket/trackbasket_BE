421378b2805a2569a7d516f3601dc318719db189
Maria Ronauli <mronauli@gmail.com>
2020-05-28 15:10:34 -0600

    add edge cases and some refactoring

--- trackbasket_be/resources/at_risk_user.py
+++ trackbasket_be/resources/at_risk_user.py
@@ -6,32 +6,32 @@ class AtRiskUsers(Resource):
   
   def get(self, id): 
     at_risk_user = AtRiskUser.find_by_id(id)
-    
-    return at_risk_user.json()
+    if at_risk_user:
+      return at_risk_user.json()
+    else: 
+      return {'data': { 'id': 'at_risk_user', 'attributes': {'error': "User not found"} } }, 400
   
   def post(self, id):
     data = request.json 
-    
-    at_risk_user = AtRiskUser(name=data['name'], 
-                              address=data['address'], 
-                              city=data['city'], 
-                              state=data['state'], 
-                              phone_number=data['phone_number'], 
-                              at_risk_user_id=id)
+    at_risk_user = AtRiskUser(at_risk_user_id=id, **data)
     at_risk_user.save_to_db()
     
     return at_risk_user.json(), 201
 
-  def put(self, id):
+  def patch(self, id):
     data = request.json 
-    
     at_risk_user = AtRiskUser.find_by_id(id)
-    at_risk_user.name = data['name']
-    at_risk_user.address = data['address']
-    at_risk_user.city = data['city']
-    at_risk_user.state = data['state']
-    at_risk_user.phone_number = data['phone_number']
     
+    if at_risk_user:
+      at_risk_user.name = data['name']
+      at_risk_user.address = data['address']
+      at_risk_user.city = data['city']
+      at_risk_user.state = data['state']
+      at_risk_user.zipcode = data['zipcode']
+      at_risk_user.phone_number = data['phone_number']
+    else: 
+      return {'data': { 'id': 'at_risk_user', 'attributes': {'error': "User not found"} } }, 400
+      
     at_risk_user.save_to_db()
     
     return at_risk_user.json()
