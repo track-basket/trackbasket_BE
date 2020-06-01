1db877b9d3e9362995bc4f9d1d295658822b3091
adumortier <42797894+adumortier@users.noreply.github.com>
2020-06-01 06:35:02 -0600

    Update krogerservice.py

--- trackbasket_be/models/krogerservice.py
+++ trackbasket_be/models/krogerservice.py
@@ -31,7 +31,6 @@ class Krogerservice:
     headers = {'Authorization': 'Bearer {}'.format(Krogerservice.return_token())}  
     
     response = requests.get('https://api.kroger.com/v1/locations?', params=parameters, headers=headers)
-    return {'status_code':   response.status_code}
     parsed_response = response.json()
 
     if response.status_code != 200:
