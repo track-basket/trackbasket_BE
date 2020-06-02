ec4426bca810e25966aefb0f4dc7ad81c0d030d4
Maria Ronauli <mronauli@gmail.com>
2020-06-02 15:25:05 -0600

    modify delete method to delete items within list
    Co-Authored-By: Alexis Dumortier <dumortier.alexis@gmail.com>

--- trackbasket_be/resources/at_risk_user.py
+++ trackbasket_be/resources/at_risk_user.py
@@ -3,6 +3,7 @@ from models.at_risk_user import AtRiskUser
 from models.basemodel import BaseModel,db 
 from models.krogerservice import Krogerservice
 from models.store import Store
+from models.item import Item
 
 class AtRiskUsers(Resource):
   
@@ -49,12 +50,15 @@ class AtRiskUsers(Resource):
     at_risk_user = db.session.query(AtRiskUser).filter_by(at_risk_user_id=id).first()
     if at_risk_user:
       for lst in at_risk_user.shopping_lists:
+        if lst.items != None:
+          for item in lst.items:
+            db.session.delete(item) 
+            db.session.commit()
         db.session.delete(lst) 
         db.session.commit()
-      db.session.commit()
       db.session.delete(at_risk_user)
       db.session.commit()
-      return {'data': { 'id': 'at_risk_user', 'attributes': {'message': "volunteer with id {} successfully deleted".format(id)} } }, 200
+      return {'data': { 'id': 'at_risk_user', 'attributes': {'message': "at_risk_user with id {} successfully deleted".format(id)} } }, 200
     else:
       return {'data': { 'id': 'at_risk_user', 'attributes': {'error': "at_risk_user does not exist"} } }, 400  
       
