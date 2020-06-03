1482d90882549ca791e6200bb95bca77282d6bca
Maria Ronauli <mronauli@gmail.com>
2020-06-03 16:17:04 -0600

    modify for env settings

--- trackbasket_be/app.py
+++ trackbasket_be/app.py
@@ -1,17 +1,18 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
 from flask_sqlalchemy import SQLAlchemy
-from models.basemodel import db
-from models.volunteer import Volunteer
-from resources.volunteer import Volunteers
-from resources.store import Stores
-from models.at_risk_user import AtRiskUser
-from resources.at_risk_user import AtRiskUsers
-from resources.shopping_list import ShoppingLists
-from models.store import Store
-from resources.items import Items
-from resources.list_shopping_lists import ListShoppingLists
+from .models.basemodel import db
+from .models.volunteer import Volunteer
+from .resources.volunteer import Volunteers
+from .resources.store import Stores
+from .models.at_risk_user import AtRiskUser
+from .resources.at_risk_user import AtRiskUsers
+from .resources.shopping_list import ShoppingLists
+from .models.store import Store
+from .resources.items import Items
+from .resources.list_shopping_lists import ListShoppingLists
 import os
+
 # POSTGRES = {
 #     'user': 'postgres',
 #     'pw': 'password',
@@ -20,23 +21,24 @@ import os
 #     'port': '5432',
 # }
 
-app = Flask(__name__)
-api = Api(app)
+def create_app(config_class):
+  app = Flask(__name__)
 
+  app.config.from_object(config_class)
+  app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:\%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+  db.init_app(app)
+  api = Api(app)
 
-db.init_app(app)
+  api.add_resource(Volunteers, '/volunteer/<string:id>')
+  api.add_resource(AtRiskUsers, '/atriskuser/<string:id>')
+  api.add_resource(Stores, '/store/<string:id>')
+  api.add_resource(Items, '/items')
 
-api.add_resource(Volunteers, '/volunteer/<string:id>')
-api.add_resource(AtRiskUsers, '/atriskuser/<string:id>')
-api.add_resource(Stores, '/store/<string:id>')
-api.add_resource(Items, '/items')
+  api.add_resource(ShoppingLists, '/shoppinglist/<string:id>')
+  api.add_resource(ListShoppingLists, '/listshoppinglists/')
 
-api.add_resource(ShoppingLists, '/shoppinglist/<string:id>')
-api.add_resource(ListShoppingLists, '/listshoppinglists/')
-
-if __name__ == '__main__':
-    app.run(port= 5000, debug= True)
+  if __name__ == '__main__':
+      app.run(port= 5000, debug= True)
+      
+  return app
