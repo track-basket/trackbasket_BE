17be4be4ab8634b24faf4903730f22c1552d870b
Alexis Dumortier <dumortier.alexis@gmail.com>
2020-06-26 17:50:22 -0600

    Add conversation and message models

---
+++ trackbasket_be/models/conversation.py
@@ -0,0 +1,33 @@
+from .basemodel import BaseModel, db
+import datetime 
+from .message import Message
+
+class Conversation(BaseModel, db.Model):
+  __tablename__ = 'conversations'
+  id = db.Column(db.Integer, primary_key=True)
+  at_risk_user_id = db.Column(db.String, db.ForeignKey('at_risk_users.id'))
+  volunteer_id = db.Column(db.String)
+  created_date = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+  messages = db.relationship('Message', backref='conversation')
+
+  @classmethod 
+  def find_by_id(cls, at_risk_user_id, volunteer_id):
+    return cls.query.filter_by(at_risk_user_id=at_risk_user_id, volunteer_id=volunteer_id).first()
+
+  @classmethod
+  def create(cls, at_risk_user_id, volunteer_id):
+    conversation = cls(volunteer_id=volunteer_id, at_risk_user_id=at_risk_user_id)
+    db.session.add(conversation)
+    db.session.commit()
+    return conversation
+
+  def add_message(self, text):
+    message = Message(text=text, conversation=self)
+    db.session.add(message)
+    db.session.commit()
+
+  def json(self):
+    return {'data': {  "id": "conversation", "attributes":
+          { "messages": [message.json() for message in self.messages] } } }
+
+
---
+++ trackbasket_be/models/message.py
@@ -0,0 +1,13 @@
+from .basemodel import BaseModel, db
+import datetime 
+
+class Message(BaseModel, db.Model):
+  __tablename__ = 'messages'
+  id = db.Column(db.Integer, primary_key=True)
+  conversation_id = db.Column(db.Integer, db.ForeignKey('conversations.id'))
+  text = db.Column(db.String)
+  author = db.Column(db.String)
+  created_date = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+  
+  def json(self):
+    return {"text": self.text, "timestamp": self.created_date.strftime("%d/%m/%Y %H:%M:%S")}
